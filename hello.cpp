e2924cac17ad792a6dc5e8b87bf1190db1df6195
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-10-10 16:50:59 +0600

    Feat: Data structure and Algorithm for Quant analysis with Cplusplus C++

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,8 @@
 #include "iostream"
- using namespace std;
+ 
 
 int main()
 {
-    cout << "Hello World! My Very First C++";
-    return 0;
+    int integer = 4543;
+    std::cout<<"The value of integer is "<<integer;
 }
