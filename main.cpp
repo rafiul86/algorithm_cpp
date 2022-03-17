d6877748daf7c2dded341ba4d1af84092a18ba4f
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-03-17 17:30:15 +0600

    Feat: Data structure and algorithm in cplusplus

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+using namespace std;
  int main()
  {
-     using namespace std;
-     cout<<"CPLUSPLUS Project setup"<<"\n";
+     int A[4];
+        A[0]=1;
+        A[1]=2;
+        A[2]=3;
+    for (int i=0;i<3; i++){
+        cout<<A[i]<<endl;
+    }
      return 0;
  }
