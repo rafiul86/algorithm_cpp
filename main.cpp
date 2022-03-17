d1d6c13241b32173934826af728cc9d28ad632f2
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-03-17 15:37:40 +0600

    Feat: Project setup

--- main.cpp
+++ main.cpp
@@ -2,12 +2,6 @@
  int main()
  {
      using namespace std;
-     cout<<"int size = "<<sizeof(int)<<"\n";
-     cout<<"short size = "<<sizeof(short)<<"\n";
-     cout<<"long size = "<<sizeof(long)<<"\n";
-     cout<<"char size = "<<sizeof(char)<<"\n";
-     cout<<"float size = "<<sizeof(float)<<"\n";
-     cout<<"double size = "<<sizeof(double)<<"\n";
-     cout<<"bool size = "<<sizeof(bool)<<"\n";
+     cout<<"CPLUSPLUS Project setup"<<"\n";
      return 0;
  }
