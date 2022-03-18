f11e7d1fe417be6a67eb7d9551eae8c383f4cc10
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-03-18 11:57:17 +0600

    while loop if statement

--- main.cpp
+++ main.cpp
@@ -1,30 +1,39 @@
-#include <iostream>
-#include <vector>
-using std::cout;
-using std::vector;
 
-void PrintBoard(vector<vector<int>> v){
-  for (auto i:v){
-  	for(auto j:i){
-    	cout << j << " ";
-    }
-    cout << "\n";
-  }
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::ifstream my_file;
+    my_file.open("hello");
+    if (my_file) {
+      std::cout << "The file stream has been created!" << "\n";
+    }    
 }
-int main() {
-  bool logic = false;
-  if (logic){
-      vector<vector<int>> board{{0, 1, 0, 0, 0, 0},
-                            {0, 1, 0, 0, 0, 0},
-                            {0, 1, 0, 0, 0, 0},
-                            {0, 1, 0, 0, 0, 0},
-                            {0, 0, 0, 0, 1, 0}};
-  PrintBoard(board);
-  } else {
-      int a = 5;
-      while (a >= 2){
-          cout << a << "\n";
-          a--;
-      }
-  }
- }
+
+// void PrintBoard(vector<vector<int>> v){
+//   for (auto i:v){
+//   	for(auto j:i){
+//     	cout << j << " ";
+//     }
+//     cout << "\n";
+//   }
+// }
+// int main() {
+//   bool logic = false;
+//   if (logic){
+//       vector<vector<int>> board{{0, 1, 0, 0, 0, 0},
+//                             {0, 1, 0, 0, 0, 0},
+//                             {0, 1, 0, 0, 0, 0},
+//                             {0, 1, 0, 0, 0, 0},
+//                             {0, 0, 0, 0, 1, 0}};
+//   PrintBoard(board);
+//   } else {
+//       int a = 5;
+//       while (a >= 2){
+//           cout << a << "\n";
+//           a--;
+//       }
+//   }
+//  }
