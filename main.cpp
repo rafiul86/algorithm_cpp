986fdca057cec7513dd00c7c295244e08b38469b
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-03-19 15:37:50 +0600

    Feat: pointer array

--- main.cpp
+++ main.cpp
@@ -18,10 +18,16 @@ using std::vector;;
 
 
 int main() {
-    long double a {1.76886648};
-    long double *p;
-    p = &a;
-    cout << "value of pointer p is " << *p << endl;
+    int *p;
+    p = new int[6];
+    p[0] = 10;
+    p[1] = 20;
+    p[2] = 30;
+    p[3] = 40;
+    p[4] = 50;
+    for (int i=0; i<6; i++) {
+        cout << p[i] << endl;
+    }
     return 0;
 }
 
