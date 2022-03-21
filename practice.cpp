0dfbb6ede0cbb13ca48b45e327ef3305fbffd809
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-03-21 13:38:08 +0600

    Feat: Counter

--- practice.cpp
+++ practice.cpp
@@ -8,11 +8,14 @@ using std::cout;
 
 int main() 
 {
-    int counter{10};
-    int result{10};
+   int num1 {4}, num2 {2}, num3 {1}, num4 {3};
+   int count {4};
+   int total = num1 + num2 + num3 + num4 ;
+   double average {0.0};
+   
+    average = static_cast<double>(total) / count;
+  
+   cout << average << "\n";
 
-    result = counter--;
-    cout << "result: " << result << "\n";
-    cout << "counter: " << counter << "\n";
 
 }
