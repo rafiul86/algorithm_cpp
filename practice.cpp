38e3b4eba2bb2bdad4f3703f966e7a274794bdd3
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-03-24 19:12:04 +0600

    Feat: Arithmatic

--- practice.cpp
+++ practice.cpp
@@ -6,20 +6,13 @@ using std::istringstream;
 using std::string;
 using namespace std;
 
-enum class Direction { North, South, East, West };
+// enum class Direction { North, South, East, West };
 
 int main () {
-    Direction my_horizon = Direction:: South;
-    switch (my_horizon) {
-
-        case Direction::North : cout << "North" << endl;
-        break;
-        case Direction::South: cout << "South is my favorite direction" << endl;
-        break;
-        case Direction::East: cout << "East" << endl;
-        break;
-        case Direction::West: cout << "West" << endl;
-        break;
+    int a, b, c;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    c = (a + b)/2;
+    cout << "The sum of " << a << " and " << b << " is " << c << endl;
         return 0;
     }
-}
