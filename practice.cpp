0b26942854c5cbf35bac2d7d49c45cc23df409a4
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-03-30 15:31:12 +0600

    Feat- factor and perfect number

--- practice.cpp
+++ practice.cpp
@@ -9,15 +9,27 @@ using namespace std;
 #include<iostream>
 using namespace std;
 
-void factor(int n){
-   for(int i=1; i<=n; i++){
-        if(n%i==0){
-        cout<<i<<" ";
+void Perfect(int n)
+{
+    int sum=0;
+    
+    for(int i=1; i<=n; i++){
+        
+        if(n%i==0)
+        {
+            sum += i;
+        }
+        
     }
-   }
+    
+    if(sum == n*2)
+        cout<<"perfect";  
+    else
+        cout<<"not perfect";
+            
 }
 
 int main(){
- factor(10);
+ Perfect(496);
 }
 
