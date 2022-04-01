10b244a1913e6d9f84a9c84ccdf7c207362c4a3e
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-01 18:57:05 +0600

    Feat: Character array

--- practice.cpp
+++ practice.cpp
@@ -12,14 +12,9 @@ using namespace std;
 
 
 int main(){
-  int A[2][3]= {{11,12,13},{14,15,16}};
-  int B[2][3]= {{21,30,44},{51,62,73}};
-  int C[2][3];
- for(int i=0;i<2;i++){
-   for(int j=0;j<3;j++){
-     C[i][j] =  B[i][j] % A[i][j];
-     cout << C[i][j] << " ";
-   }
- }
+  char s[20];
+  cout<<"Enter your name: ";
+  cin.get(s,20);
+  cout<<"Hello "<<s<<endl;
 }
 
