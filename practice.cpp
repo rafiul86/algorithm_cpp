711356b41ceed1ae64d92c77d43f7fe69176bd53
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-01 10:38:40 +0600

    Feat: 2D array

--- practice.cpp
+++ practice.cpp
@@ -12,9 +12,13 @@ using namespace std;
 
 
 int main(){
- for(int i=0;i<=3;i++){
-   for(int j=0;j<=3;j++){
-     cout<<i<<" "<<j<<endl;
+  int A[2][3]= {{11,12,13},{14,15,16}};
+  int B[2][3]= {{21,30,44},{51,62,73}};
+  int C[2][3];
+ for(int i=0;i<2;i++){
+   for(int j=0;j<3;j++){
+     C[i][j] =  B[i][j] % A[i][j];
+     cout << C[i][j] << " ";
    }
  }
 }
