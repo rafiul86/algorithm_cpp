a410834f663eae5611b43365af571ef7a6e562e5
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-06 13:06:38 +0600

    Feat: string concatenation

--- main.cpp
+++ main.cpp
@@ -10,19 +10,42 @@ using std::string;
 using std::vector;
 
 
-
-vector<vector<int>> ReadBoardFile(string path){
-
+vector<int> ParseLine(string line) {
+    istringstream sline(line);
+    int n;
+    char c;
+    vector<int> row;
+    while (sline >> n >> c && c == ',') {
+      row.push_back(n);
+    }
+    return row;
 }
 
-void PrintBoard(const vector<vector<int>> board){
-
+vector<vector<int>> ReadBoardFile(string path) {
+  ifstream myfile (path);
+  vector<vector<int>> board{};
+  if (myfile) {
+    string line;
+    while (getline(myfile, line)) {
+      vector<int> row = ParseLine(line);
+      board.push_back(row);
+    }
+  }
+  return board;
 }
 
- 
-
+void PrintBoard(const vector<vector<int>> board) {
+  for (int i = 0; i < board.size(); i++) {
+    for (int j = 0; j < board[i].size(); j++) {
+      cout << board[i][j];
+    }
+    cout << "\n";
+  }
+}
 
 int main() {
-  auto board = ReadBoardFile("1.board");
-  PrintBoard(board);
+ string s = "Happy";
+ string s2 = "Holiday";
+ string s3 = s + ' ' + s2;
+ cout<< s3 << "\n";
 }
