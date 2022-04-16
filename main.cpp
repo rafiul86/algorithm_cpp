027423d8ee3a6e568e072f38a3029d5d61ec3a04
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-16 15:01:29 +0600

    Feat: Filestream

--- main.cpp
+++ main.cpp
@@ -6,16 +6,37 @@
 using std::cout;
 using std::cin;
 using std::endl;
+using std::ofstream;
 using std::ifstream;
 using std::istringstream;
 using std::string;
 using std::vector;
-
-
+#define PI 3.1416
+class Add {
+  public:
+  int fun(int &a, int &b) {
+    a++;
+    b++;
+    return a + b;
+  }
+};
 
 int main(){
-  int x = 10;
-  int * p = &x;
-  ++x;
-  cout << *p <<" "<< &x<< endl;
+  string name;
+  int roll;
+  string branch;
+ ofstream file("./files/board.txt");
+ if(file){
+    cout<<"File created"<<endl;
+    cin>>name>>roll>>branch;
+  }
+  else{
+    cout<<"File not created"<<endl;
+ }
+ ifstream my_file("./files/board.txt");
+ if(my_file){
+   my_file>>name>>roll>>branch;
+   my_file.close();
+   cout<<name<<" "<<roll<<" "<<branch<<endl;
+ }
 }
