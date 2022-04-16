3486d92196702c8d1d275cae6c172515d6f11d9a
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-16 11:23:43 +0600

    Constant

--- main.cpp
+++ main.cpp
@@ -13,20 +13,9 @@ using std::vector;
 
 
 
-class Base{
-  public:
-    Base(){cout<< "Non-paramiterized Base constructor"<<endl;}
-    Base(int x){cout<< "Paramiterized Base constructor: "<<x<< endl;}
-};
-
-class Derived : public Base{
-  public:
-    Derived(){cout<< "Non-paramiterized Derived constructor"<<endl;}
-    Derived(int y){cout<< "Paramiterized Derived constructor: "<<y<< endl;}
-    Derived(int x, int y):Base(x)
-    {cout<< "Paramiterized Derived constructor and Base: "<<x<<" "<<y<< endl;}
-};
-
 int main(){
-  Derived D(5,2);
+  int x = 10;
+  int * p = &x;
+  ++x;
+  cout << *p <<" "<< &x<< endl;
 }
