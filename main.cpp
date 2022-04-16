1dd7ee774890f470724c56f0b390339cd6e433cb
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-16 19:00:48 +0600

    lambda function

--- main.cpp
+++ main.cpp
@@ -7,36 +7,13 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::ofstream;
+using std::ios;
 using std::ifstream;
 using std::istringstream;
 using std::string;
 using std::vector;
 #define PI 3.1416
-class Add {
-  public:
-  int fun(int &a, int &b) {
-    a++;
-    b++;
-    return a + b;
-  }
-};
-
 int main(){
-  string name;
-  int roll;
-  string branch;
- ofstream file("./files/board.txt");
- if(file){
-    cout<<"File created"<<endl;
-    cin>>name>>roll>>branch;
-  }
-  else{
-    cout<<"File not created"<<endl;
- }
- ifstream my_file("./files/board.txt");
- if(my_file){
-   my_file>>name>>roll>>branch;
-   my_file.close();
-   cout<<name<<" "<<roll<<" "<<branch<<endl;
- }
+  int f = [](int x, int y){return x+y;}(2,3);  
+  cout<<f;
 }
