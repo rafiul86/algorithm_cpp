587a9e88059e6eeb60e4529e192498792a8bd849
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-17 20:26:53 +0600

    Derived and base class

--- main.cpp
+++ main.cpp
@@ -13,7 +13,20 @@ using std::istringstream;
 using std::string;
 using std::vector;
 #define PI 3.1416
+
+class Base {
+  public:
+  Base() { cout << "Base" << endl; }
+  Base(int x){cout << "Base x: "<< x<< endl;}
+};
+
+class Derived: public Base {
+  public:
+  Derived() { cout << "Derived" << endl; }
+  Derived(int y){cout<<"Derived y: " <<y<<endl;}
+  Derived(int x, int a):Base(a){cout<<"Base and Derived paramiterized: x " <<x <<" a: " <<a<<endl;}
+};
 int main(){
-  int f = [](int x, int y){return x+y;}(2,3);  
-  cout<<f;
+  Derived d(2, 3);
+
 }
