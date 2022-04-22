0bf626f6c2fe030c3ee96768f767363dfb7b06e9
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-22 14:25:30 +0600

    Feat: C++

--- main.cpp
+++ main.cpp
@@ -1,43 +1,11 @@
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-#include<assert.h>
+#include<stdio.h>
 
-using std::cout;
-using std::cin;
-using std::endl;
-using std::ofstream;
-using std::ios;
-using std::ifstream;
-using std::istringstream;
-using std::string;
-using std::vector;
-#define PI 3.1416
 
-class Animal {
-  public:
-   virtual void Talk () const = 0;
-};
 
-class Wolf:  public Animal  {
-  public:
-   void Talk () const override{
-      cout << "Loud" << endl;
-   };
-};
 
-class Dog: public Animal {
-  public:
-    void Talk() const override{
-      cout << "Silence" << endl;
-    }
-};
-
-int main(){
-  Dog d;
-  Wolf w;
-  d.Talk();
-  w.Talk();
+int main()
+{
+int a{5};
+int b=a*42;
+printf("%d",b);
 }
