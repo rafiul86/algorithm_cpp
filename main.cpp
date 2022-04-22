a8b439ecfdead3073bd11634e5b1a7cdfcc59361
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-22 22:33:12 +0600

    Factor

--- main.cpp
+++ main.cpp
@@ -14,24 +14,13 @@ using namespace std;
 
 
 int main(){
-  char selection=' ';
- while(selection != 'q' &&  selection != 'Q') {
-    cout << "----------------------------------- Enter your selection -----------------------------------" << endl;
-    
-    cout << "1. Choose One" << endl;
-    cout << "2. Choose Two " << endl;
-    cout << "3. Choose Three" << endl;
-    cout << "Q. Exit" << endl;
-    cin >> selection;
-    
-   if (selection == '1'){
-      cout << "You chosed one" << endl;
-    } else if (selection == '2'){
-      cout << "You chosed two" << endl;
-    } else if (selection == '3'){
-      cout << "You chosed three" << endl;
-    } else if (selection == 'q' || selection == 'Q'){
-      cout << "Exit" << endl;
-    } 
-}
+  int a[6][5] {};
+
+  for (int row = 1; row <= 10; row++){
+    for(int col = 1; col <= 10 ; col++){
+      cout << row << " * " << col << " = " << row*col << endl;
+    }
+    cout<< "-------------" << endl;
+  }
+  cout<< endl;
 }
