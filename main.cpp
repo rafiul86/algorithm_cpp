57bae100d9510befb852d9a16bedc7ae29a79c9c
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-22 22:03:03 +0600

    do while loop

--- main.cpp
+++ main.cpp
@@ -11,31 +11,27 @@
 using namespace std;
 
 
-class Animal {
-  public:
-    virtual void Talk () const = 0;
-};
 
-class Wolf:  public Animal  {
-  public:
-   void Talk () const {
-      cout << "Loud" << endl;
-   };
-   void Walk() const {
-      cout << "Walking" << endl;
-   };
-};
-
-class Dog: public Animal {
-  public:
-    void Talk() const {
-      cout << "Silence" << endl;
-    }
-};
 
 int main(){
-  Dog d;
-  Wolf w;
-  d.Talk();
-  w.Talk();
+  char selection=' ';
+ while(selection != 'q' &&  selection != 'Q') {
+    cout << "----------------------------------- Enter your selection -----------------------------------" << endl;
+    
+    cout << "1. Choose One" << endl;
+    cout << "2. Choose Two " << endl;
+    cout << "3. Choose Three" << endl;
+    cout << "Q. Exit" << endl;
+    cin >> selection;
+    
+   if (selection == '1'){
+      cout << "You chosed one" << endl;
+    } else if (selection == '2'){
+      cout << "You chosed two" << endl;
+    } else if (selection == '3'){
+      cout << "You chosed three" << endl;
+    } else if (selection == 'q' || selection == 'Q'){
+      cout << "Exit" << endl;
+    } 
+}
 }
