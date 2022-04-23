0ef13e46e58337f3b81146fcc1445a9d8376a80e
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-23 17:47:53 +0600

    Template

--- main.cpp
+++ main.cpp
@@ -8,26 +8,42 @@
 #include<ctime>
 #include<cstdio>
 
-
 using namespace std;
+
+
 
 template<class T>
 class Rectangle {
 public:
   T length;
   T width;
+Rectangle();
 
-Rectangle(T l, T w) {
-  this->length = l;
-  this->width = w;
-}
 
 T area();
 T perimeter();
+T getLength();
+T getWidth();
+void setLength(T l);
+void setWidth(T w);
+
 
 ~Rectangle(){
   cout << "Destructor called" << endl;
   };
+};
+
+template<class T>
+Rectangle<T>:: Rectangle(): length(5), width(6){};
+
+template<class T>
+void Rectangle<T>:: setLength(T l){
+  length = l;
+};
+
+template<class T>
+void Rectangle<T>:: setWidth(T w){
+  width = w;
 };
 
 template<class T>
@@ -36,17 +52,29 @@ T Rectangle<T>:: area(){
 };
 
 template<class T>
+T Rectangle<T>:: getLength(){
+  return length;
+};
+
+template<class T>
+T Rectangle<T>:: getWidth(){
+  return width;
+};
+
+
+template<class T>
 T Rectangle<T>:: perimeter(){
   return 2*(length+width);
 };
 
 int main(){
- int l,w;
- cin >> l >> w;
- Rectangle<int> r(l, w);
- r.length = l;
- r.width = w;
- cout << r.area() << endl;
- cout << r.perimeter() << endl;
- return 0;
+int len;
+int wid;
+Rectangle<int> r;
+cin >> len >> wid;
+r.setLength(len);
+r.setWidth(wid);
+cout << r.area() << endl;
+cout << r.perimeter() << endl;
+return 0;
 }
