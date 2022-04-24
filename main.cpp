bccf2b8336049c7ff82754df39371e9385c77a8e
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-24 14:38:28 +0600

    String decipher

--- main.cpp
+++ main.cpp
@@ -1,86 +1,24 @@
-#include<fstream>
-#include<iostream>
-#include<string>
-#include<vector>
-#include<algorithm>
-#include<cmath>
-#include<cstdlib>
-#include<ctime>
-#include<cstdio>
-
+#include <iostream>
+#include <string>
 using namespace std;
 
+int main() {
+    
+    string message;
+    string encrypted_message;
+    string decrypted_message;
+    getline(cin, message);
+    for(int i=0; i<= message.length(); i++){
+        char decipher_letter = message[i] -1;
+        encrypted_message.push_back(decipher_letter);
+    }
 
-
-template<class T>
-class Rectangle {
-public:
-  T length;
-  T width;
-Rectangle();
-
-
-T area();
-T perimeter();
-T getLength();
-T getWidth();
-void setLength(T l);
-void setWidth(T w);
-
-
-~Rectangle(){
-  cout << "Destructor called" << endl;
-  };
-};
-
-template<class T>
-Rectangle<T>:: Rectangle(): length(5), width(6){};
-
-template<class T>
-void Rectangle<T>:: setLength(T l){
-  length = l;
-};
-
-template<class T>
-void Rectangle<T>:: setWidth(T w){
-  width = w;
-};
-
-template<class T>
-T Rectangle<T>:: area(){
-  return length*width;
-};
-
-template<class T>
-T Rectangle<T>:: getLength(){
-  return length;
-};
-
-template<class T>
-T Rectangle<T>:: getWidth(){
-  return width;
-};
-
-
-template<class T>
-T Rectangle<T>:: perimeter(){
-  return 2*(length+width);
-};
-
-int main(){
-
-string s1 = "Hello";
-string s2 = {"hello"};
-string s3 = {"HELLO", 3};
-string s4 = {"HELLO", 0, 3};
-
-string s6 = {s1};
-cout << s1 << endl;
-cout << s2 << endl;
-cout << s3 << endl;
-cout << s4 << endl;
-
-cout << s6 << endl;
-
-return 0;
+    for(int i=0; i<= encrypted_message.length(); i++){
+        char decipher_letter = encrypted_message[i] + 1;
+        decrypted_message.push_back(decipher_letter);
+    }
+    
+    cout << encrypted_message << endl;
+    
+    cout << decrypted_message << endl;
 }
