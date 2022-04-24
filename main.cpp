088b2ee7aad7adfb9347eb15a116e1046b942e55
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-24 14:57:40 +0600

    String decipher

--- main.cpp
+++ main.cpp
@@ -1,24 +1,11 @@
 #include <iostream>
 #include <string>
+#include<cmath>
 using namespace std;
 
 int main() {
-    
-    string message;
-    string encrypted_message;
-    string decrypted_message;
-    getline(cin, message);
-    for(int i=0; i<= message.length(); i++){
-        char decipher_letter = message[i] -1;
-        encrypted_message.push_back(decipher_letter);
-    }
-
-    for(int i=0; i<= encrypted_message.length(); i++){
-        char decipher_letter = encrypted_message[i] + 1;
-        decrypted_message.push_back(decipher_letter);
-    }
-    
-    cout << encrypted_message << endl;
-    
-    cout << decrypted_message << endl;
+    cout<< sqrt(400) << endl;
+    double result = pow(2.1,3.2);
+    cout << result << endl;
+    return 0;
 }
