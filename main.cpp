685a425805f93bfc011eb60c54499172d92e4568
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-25 21:08:28 +0600

    Array

--- main.cpp
+++ main.cpp
@@ -3,21 +3,22 @@
 #include<cmath>
 using namespace std;
 
-int fun(int n){
-  return n/2;
+int print_array( int my_num[], int size){
+  for(int i=0; i<size ;i++){
+    
+    cout<<my_num[i]<<" ";
+  }
+  cout<<endl;
+  return 0;
 }
 
-int fun(int n, int m){
-  return n*m;
-}
-
-int fun(int n, int m, int o){
-  int x = n + m + o;
-  return x;
-}
 
 int main() {
-  cout << "Division : " << fun(6) << endl;
-  cout << "Multiplication : " <<  fun(5, 10) << endl;
-  cout << "Addition : " << fun(5, 10, 15) << endl;
+  int arr[5] = {1,2,3,4,5};
+  print_array(arr, 5);
+  for(int i=0; i<5 ;i++){
+    arr[3] = 333;
+    cout<<arr[i]<<" ";
+  }
+  return 0;
 }
