76bf989bd1d7c9d0346af5a6c38a69efd5fa07f0
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-05-20 17:22:42 +0600

    vector and array differentiation

--- main.cpp
+++ main.cpp
@@ -4,13 +4,16 @@
 #include <thread>
 #include <vector>
 
+/* ENTRY POINT 
+OF THE PROGRAM */
 
 
 
 int main(){
-int fav_num;
-std::cout << "Enter your favorite number: ";
-std::cin >> fav_num;
-std::cout << "Your favorite number is " << fav_num << std::endl;
+std:: vector<int> my_array;  // fixed size array
+for (int i= 0 ; i < 10 ; i++){
+    my_array.push_back(i);
+    std::cout << my_array[i] << std::endl;
+}
 return 0;
 }
