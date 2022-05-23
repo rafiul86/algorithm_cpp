41c0f24d51ce3bd3265f963b987ded2dbb7a5cc6
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-05-23 21:09:27 +0600

    referrence

--- main.cpp
+++ main.cpp
@@ -4,7 +4,11 @@
 #include <cmath>
 #include <thread>
 #include <vector>
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ifstream;
+using std::string;
 /* ENTRY POINT 
 OF THE PROGRAM */
 
@@ -15,9 +19,14 @@ struct Person {
 } r, r2, r3;
 
 int main(){
-int a = 11;
-int *p = &a;
-cout << &a << endl;
-cout << p << endl;
+
+int a = 10;
+int &r = a;
+cout << a << " and " << r << endl;
+int b = 20;
+r = b;
+r = 30;
+cout << a << " and " << r << endl;
+cout << a << " and " << b <<  " and " << r << endl;
 return 0;
 }
